ee12fc8e2117d3baf636e526ce0d328c16afaefe
Shashank290798 <shashanknalogal@gmail.com>
2022-10-18 14:40:17 +0530

    task 17 commit

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize')
 
-const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'node-complete',
-    password: 'Shashank@143'
+const sequelize = new Sequelize('node-complete', 'root', 'Shashank@143',{
+    dialect: 'mysql',
+    host:'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
